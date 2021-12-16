d166bfee1c5a83339bc584b06cdaddc23fb977bf
kezhao <2667422922@qq.com>
2021-12-16 17:22:13 +0800

    with

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import './App.css';
 import {Button} from "antd";
 import {DatePicker} from 'antd';
+import {WechatOutlined} from '@ant-design/icons';
+
+const { RangePicker } = DatePicker;
 class App extends Component {
 
     state = {
@@ -18,7 +21,9 @@ class App extends Component {
                 <div className="container">
                     <Button type="primary">Primary Button</Button>
                     <DatePicker onChange={this.onChange}/>
+                    <RangePicker />
                     <div style={{marginTop: 16}}>{this.state.date1}</div>
+                    <WechatOutlined />
                 </div>
             </div>
         );
